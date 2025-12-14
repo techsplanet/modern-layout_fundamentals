efdfa992cb6c5f24fd4e9303186881e926e15302
Naman <namansingh99694@gmail.com>
2025-12-14 11:37:34 +0530

    implete basic grid layout

---
+++ app/components/BasicGrid.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const BasicGrid = () => {
+  return (
+    <div className='show-container' id='parent'>
+      <div className="item-2">
+        <h1>footer</h1>
+      </div>
+      <div className="item-3">
+        <h1>header</h1>
+      </div>
+      <div className="item-4">
+        <h1>sidebar</h1>
+      </div>
+      <div className="item-1">
+        <h1>main</h1>
+      </div>
+    </div>
+  )
+}
+
+export default BasicGrid
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import BasicGrid from "./components/BasicGrid"
 
 const page = () => {
   return (
-    <div>page</div>
+    <BasicGrid />
   )
 }
 
